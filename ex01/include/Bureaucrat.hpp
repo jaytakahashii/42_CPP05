2628417cec1899e7cad8491fc147f8cace3de048
Jay Takahashi <qsakura14555@gmail.com>
2025-05-09 17:37:46 +0900

    feat(SRC): ✨  initialize ex01

---
+++ ex01/include/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+
+class Bureaucrat {
+ private:
+  const std::string _name;
+  int _grade;
+
+ public:
+  // === OCF ===
+
+  Bureaucrat();
+  ~Bureaucrat();
+  Bureaucrat(const Bureaucrat &src);
+  Bureaucrat &operator=(const Bureaucrat &src);
+
+  // === Constructors ===
+
+  Bureaucrat(const std::string &name, int grade);
+
+  // === Getters ===
+
+  const std::string &getName() const;
+  int getGrade() const;
+
+  // === Operators ===
+
+  void incrementGrade();
+  void decrementGrade();
+
+  // === Exceptions ===
+
+  class GradeTooHighException : public std::exception {
+   public:
+    const char *what() const throw();
+  };
+
+  class GradeTooLowException : public std::exception {
+   public:
+    const char *what() const throw();
+  };
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
