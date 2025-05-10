0888066825d734884e25cafcdc95a812e8c9200a
jtakahas <jtakahas@student.42tokyo.jp>
2025-05-10 17:57:52 +0900

    refactor(SRC): ♻️  ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -2,23 +2,32 @@
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
+#include "color.hpp"
 
 int main() {
   try {
+    std::cout << YELLOW << "=== Bureaucrat ===" << RESET << std::endl;
     Bureaucrat b1("Alice", 50);
-    Form f1("tokyo", 45, 30);
-
     std::cout << b1 << std::endl;
+
+    std::cout << YELLOW << "\n=== Form ===" << RESET << std::endl;
+    Form f1("tokyo", 45, 30);
     std::cout << f1 << std::endl;
 
-    b1.signForm(f1);  // 署名失敗（grade不足）
+    std::cout << YELLOW << "\n=== signForm ===" << RESET << std::endl;
+    b1.signForm(f1);
+    std::cout << f1 << std::endl;
+
+    std::cout << YELLOW << "\n=== incrementGrade ===" << RESET << std::endl;
     for (int i = 0; i < 10; ++i)
       b1.incrementGrade();
-
     std::cout << b1 << std::endl;
-    b1.signForm(f1);  // 署名成功
+
+    std::cout << YELLOW << "\n=== signForm ===" << RESET << std::endl;
+    b1.signForm(f1);
     std::cout << f1 << std::endl;
 
+    std::cout << YELLOW << "\n=== fin ===" << RESET << std::endl;
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
